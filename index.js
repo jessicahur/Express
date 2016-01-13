3c706481da28b0c19af1af8e22cba3100c1155a9
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-13 13:59:07 -0800

    working basic logics. Need to determine a way to name files

---
+++ index.js
@@ -0,0 +1,5 @@
+var app = require('./lib/app');
+
+app.listen(9000, function() {
+  console.log('Server has started. Listening on port 9000....');
+});
---
+++ test/test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('EXPRESS Server With Simple Persistence', function() {
+
+  it('should ')
+
+});
