adb7efd488ff7dea6031c271c2f82c6a4eaf83cf
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-15 13:51:25 -0800

    clean up some white spaces

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var router = require('./router');
 
 var app = express();
 
-
 app.use('/notes',router);
+
+app.use(function(req, res, next) {
+  res.send('404, no page found: '+req.url);
+});
 
 module.exports = app;
